89bb926f31415015b7582bffead816e545d8e2d1
matalv <matalv@ttu.ee>
2021-09-25 19:39:18 +0300

    added morale component

---
+++ src/app/components/morale/morale.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-morale',
+  template: `
+    <p>
+      morale works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class MoraleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
