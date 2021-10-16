cfb1f1b0cd9d1bdcd6404132e0b4d6c6cc77beee
Markus Talvik <matalv@ttu.ee>
2021-10-16 22:01:33 +0300

    both search bars show same stock, everything else is working

--- src/app/components/compare-stock/compare-stock.component.ts
+++ src/app/components/compare-stock/compare-stock.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {WebRequestService} from "../../web-request.service";
+import {TaskService} from "../../task.service";
+import {StockInterface} from "../../Interface/StockInterface";
+import {StockGuiServiceService} from "../stock-graphical-interface/stock-graphical-interface/stock-gui-service.service";
+
 
 @Component({
   selector: 'app-compare-stock',
@@ -7,10 +12,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CompareStockComponent implements OnInit {
 
-  constructor() { }
+  constructor(private  apiService: WebRequestService, private taskService: TaskService,
+              public stockGUIService: StockGuiServiceService) {
+  }
+
+  hasSubmitted: boolean = false;
+  stockInterface!: StockInterface
+  dataSource = [{"symbol": "", "open": 0, "close": 0,"high": 0,"volume": 0,"lastDate": "", "stockDateInfo": [""],
+    "stockCloseInfo": [0]}];
+
+
+  getData = (symbol: string) => {
+    this.taskService.getStock(symbol.toUpperCase()).subscribe((data) => {
+      this.stockInterface = data
+      this.stockGUIService.buildStockInfoWithInterface(this.stockInterface)
+      this.dataSource = this.stockGUIService.dataSource
+      this.hasSubmitted = true;
+    });
+    // Need to implement something for the case where the given stock symbol doesn't exist
+  }
+
+  getStockData = (symbol: string) => {
+    this.taskService.getStock(symbol.toUpperCase()).subscribe((data) => {
+      this.stockInterface = data
+      this.stockGUIService.buildStockInfoWithInterface(this.stockInterface)
+      this.dataSource = this.stockGUIService.dataSource
+      this.hasSubmitted = true;
+    });
+    // Need to implement something for the case where the given stock symbol doesn't exist
+  }
 
   ngOnInit(): void {
   }
 
 }
-
