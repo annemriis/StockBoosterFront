07b67ee0bbdf672b41954f4ed7a9d54b0ec132c5
Peeter Tarvas <peetertarvas@gmail.com>
2021-11-09 22:10:14 +0200

    this is very depressing

--- scripts/certbot.sh
+++ scripts/certbot.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 sudo docker run -it --rm --name certbot \
-        -v "/certbot/www/:/var/www/certbot/" \
-        -v "/var/lib/letsencrypt:/var/lib/letsencrypt" \
-        -v "/certbot/conf/:/etc/letsencrypt/"
+        -v "/cert/www/:/var/www/certbot/" \
+        -v "/cert/conf/:/etc/letsencrypt/"
         certbot/certbot certonly \
         certonly --webroot \
         --register-unsafely-without-email --agree-tos \
