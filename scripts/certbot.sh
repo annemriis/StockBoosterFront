d21003f8a7ffb37169061e8c2a7bf78ad1943551
Peeter Tarvas <peetertarvas@gmail.com>
2021-11-09 21:45:10 +0200

    try to get certbot working with script now

--- scripts/certbot.sh
+++ scripts/certbot.sh
@@ -4,4 +4,9 @@ sudo docker run -it --rm --name certbot \
         -v "/certbot/www/:/var/www/certbot/" \
         -v "/var/lib/letsencrypt:/var/lib/letsencrypt" \
         -v "/certbot/conf/:/etc/letsencrypt/"
-        certbot/certbot certonly
+        certbot/certbot certonly \
+        certonly --webroot \
+        --register-unsafely-without-email --agree-tos \
+        --webroot-path=/data/letsencrypt \
+        --staging \
+        -d stockbooster.ml
