2de02e72392253c27e60602e132bf506c3664cbf
Kaisa-Mari Veinberg <kaisamari.veinberg@gmail.com>
2021-12-15 11:48:17 +0200

    Error message for compare stock page

--- src/app/service/stock-gui-service.service.ts
+++ src/app/service/stock-gui-service.service.ts
@@ -67,6 +67,7 @@ export class StockGuiServiceService {
         "lastDate": this.lastDate ? this.lastDate : "", "stockDateInfo": this.stockDateInfo ? this.stockDateInfo : [],
         "stockCloseInfo": this.stockCloseInfo ? this.stockCloseInfo : [],}];
       this.isBuilding();
+      this.gotResponse = true;
       return this;
     } else {
       this.gotResponse = false;
