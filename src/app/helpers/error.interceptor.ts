7e8f0f910621f9eb3ea4faecb37a4c9976bfac93
Kaisa-Mari Veinberg <kaisamari.veinberg@gmail.com>
2021-12-17 19:25:23 +0200

    Remove logging out with 401 error

--- src/app/helpers/error.interceptor.ts
+++ src/app/helpers/error.interceptor.ts
@@ -7,18 +7,18 @@ import {
 } from '@angular/common/http';
 import {Observable, throwError} from 'rxjs';
 import {catchError} from "rxjs/operators";
-import {AuthenticationService} from "../service/authentication.service";
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
 
-  constructor(private authenticationService: AuthenticationService) {}
+  constructor() {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     return next.handle(request).pipe(catchError(err => {
       if (err.status == 401) {
         console.log("Unauthorized!")
-        this.authenticationService.logout();
+        //this.authenticationService.logout();
+        // would be ok if exceeding api request limit didn't cause 401 error as well, don't want to log out because of it
       }
 
       const error = err.error.message || err.statusText;
